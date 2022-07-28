1fa5cae11cafb71da3c85054e782361313ea607d
Ritin Malhotra <rm5486@nyu.edu>
2022-07-28 18:39:06 +0530

    Add Solutions for Ex.2 to Fork

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
+const repeatString = function (string, num) {
+    if (num < 0) return 'ERROR'
+    let answer = "";
+    while (num > 0) {
+        answer += string;
+        num--;
+    }
 
+    return answer;
 };
 
 // Do not edit below this line
