0f10b2d5eed0ee656b167fb899f9ecdddbdc8354
Ritin Malhotra <rm5486@nyu.edu>
2022-07-28 19:12:49 +0530

    Add Ex.4 Solutions to Fork

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...toBeRemoved) {
+    let toBeRemovedIndex = 0;
+    while (toBeRemovedIndex < toBeRemoved.length) {
+        for (let i = 0; i < array.length; i++) {
+            if (array[i] === toBeRemoved[toBeRemovedIndex]) array.splice(i, 1);
+        }
+        toBeRemovedIndex++;
+    }
 
+    return array;
 };
 
 // Do not edit below this line
