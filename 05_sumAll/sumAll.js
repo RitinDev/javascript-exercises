e7934172cd245fc7ef9ea9a0ba3ed6ce7f9b7fa9
Ritin Malhotra <rm5486@nyu.edu>
2022-07-28 19:30:20 +0530

    Add Ex.5 Solutions to Fork

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+    if (!(typeof (num1) == 'number') || num1 < 0) return 'ERROR';
+    if (!(typeof (num2) == 'number') || num2 < 0) return 'ERROR';
 
+    let smallerNum = Math.min(num1, num2);
+    let largerNum = Math.max(num1, num2);
+    let sum = 0;
+
+    for (let i = smallerNum; i <= largerNum; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
