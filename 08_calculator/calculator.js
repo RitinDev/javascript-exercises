3f1bf322f027aa4e789ce8c6bfe2fc2bca5b3b79
Ritin Malhotra <rm5486@nyu.edu>
2022-08-03 23:47:39 +0530

    Add Ex.8 Solutions to Fork

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function (num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function (num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function (nums) {
+  let sum = 0;
+  for (let num of nums) sum += num;
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function (nums) {
+  let res = 1;
+  for (const num of nums) res *= num;
+  return res;
 };
 
-const power = function() {
-	
+const power = function (num1, num2) {
+  return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  let res = 1;
+  for (let i = 1; i <= num; i++) {
+    res *= i;
+  }
+
+  return res;
 };
 
 // Do not edit below this line
