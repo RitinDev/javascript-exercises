b9587140275b5249d2877d5036ba6643ca06d989
Ritin Malhotra <rm5486@nyu.edu>
2022-08-04 01:27:42 +0530

    Add Ex.12 Solutions to Fork

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+    const currentYear = new Date().getFullYear();
+    let highestAge = 0;
+    let oldestPerson;
 
+    for (const person of people) {
+        let age;
+        if (person['yearOfDeath']) age = person['yearOfDeath'] - person['yearOfBirth'];
+        else age = currentYear - person['yearOfBirth'];
+
+        if (age <= highestAge) continue;
+
+        highestAge = age;
+        oldestPerson = person;
+    }
+
+    return oldestPerson;
 };
 
 // Do not edit below this line
