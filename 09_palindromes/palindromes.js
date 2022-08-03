1d3a098a290367c46f09a4caaec5f715c6ed349b
Ritin Malhotra <rm5486@nyu.edu>
2022-08-04 01:03:01 +0530

    Add Ex.9 Solutions to Fork

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,25 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    str = str.toLowerCase();
+    let left = 0;
+    let right = str.length - 1;
+    while (left < right) {
+        if (str.charAt(left) < 'a' || str.charAt(left) > 'z') {
+            left++;
+            continue;
+        }
 
+        if (str.charAt(right) < 'a' || str.charAt(right) > 'z') {
+            right--;
+            continue;
+        }
+
+        if (str.charAt(left) != str.charAt(right)) return false;
+
+        left++;
+        right--;
+    }
+
+    return true;
 };
 
 // Do not edit below this line
