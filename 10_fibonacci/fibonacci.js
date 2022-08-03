4e1e59b05449c5e5583de8db5c7acac2ba05fd32
Ritin Malhotra <rm5486@nyu.edu>
2022-08-04 01:09:17 +0530

    Add Ex.10 Solutions to Fork

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function (fibPosition) {
+    if (fibPosition <= 0) return "OOPS";
+    let a = 1;
+    let b = 1;
+    if (fibPosition == 1) return a;
+    if (fibPosition == 2) return b;
+    let count = 3;
+    let c = 0;
+    while (count <= fibPosition) {
+        c = a + b;
+        a = b;
+        b = c;
+        count++;
+    }
 
+    return c;
 };
 
 // Do not edit below this line
